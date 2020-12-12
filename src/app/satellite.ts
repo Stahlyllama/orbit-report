5736ce23363b77176a7872c6d0a3bb68083d22cb
Kim Stahl <kim.stahl@gmail.com>
2020-12-12 02:12:28 -0600

    Lesson 1-4 are locked in and loaded.  Left off on the beginning of 5.

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,4 +4,13 @@ export class Satellite {
   type: string;
   operational: boolean;
   launchDate: string;
-constructor (name: string, type: string, launchDate: string, orbitType: string)
+//constructor is now inside our class, is now explicitly defined. 
+//one major TS diff is we need to declaere the class properties and their respective types upfront typically you'd plac these at the top of the class.  
+  constructor (name:string, type:string, launchDate:string, orbitType:string,  operational:boolean){
+      this.name= name;
+      this.type= type;
+      this.launchDate= launchDate;
+      this.orbitType= orbitType;
+      this.operational= operational;
+  }
+}
