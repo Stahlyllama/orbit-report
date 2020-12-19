e17e0949de5fab6b4f8af9869909e31e3be724cc
Kim Stahl <kim.stahl@gmail.com>
2020-12-19 16:59:17 -0600

    now I definitely finished the bonus

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { Satellite} from '../satellite';
 
 @Component({
   selector: 'app-orbit-counts',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orbit-counts.component.css']
 })
 export class OrbitCountsComponent implements OnInit {
-
+  @Input() satellites:Satellite[]; 
   constructor() { }
 
   ngOnInit() {
   }
+satelliteTypeCount (type){
+return this.satellites.filter (o => o.type === type).length
+//return calling the satelite objecgt using filter method on the satellite method in the parameter iof the filter method we create a arrow function, comparing the name variable we are passing in and comparing to see if its the same as the paramenter   (.length gives us length)  within the filter method 
 
+}
 }
