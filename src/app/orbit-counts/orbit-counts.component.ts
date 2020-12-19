f587ee113a26fd057f2ab20e2c020a9f26925d8d
Kim Stahl <kim.stahl@gmail.com>
2020-12-19 06:54:32 -0600

    still working on the bonus, but wanted to commit and turn in my assignment 6

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Satellite } from '../satellite';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-orbit-counts',
@@ -7,20 +6,10 @@ import { Satellite } from '../satellite';
   styleUrls: ['./orbit-counts.component.css']
 })
 export class OrbitCountsComponent implements OnInit {
-  @Input() satellites:Satellite[]; 
+
   constructor() { }
 
   ngOnInit() {
   }
-sort(column: string): void {
-  // array.sort modifies the array, sorting the items based on the given compare function
-  this.satellites.sort(function(a: Satellite, b: Satellite): number {
-     if(a[column] < b[column]) {
-        return -1;
-     } else if (a[column] > b[column]) {
-        return 1;
-     }
-     return 0;
-  });
-}
+
 }
